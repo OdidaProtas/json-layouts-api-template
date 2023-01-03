a0496dcd711540c8232cddc864fb3570b75fde86
dreamner <59496506+dreamner@users.noreply.github.com>
2023-01-03 08:52:29 +0000

    import roboto

--- api-template/pages/_app.tsx
+++ api-template/pages/_app.tsx
@@ -12,6 +12,9 @@ const App = ({ Component, pageProps }: AppProps) => {
         <Component {...pageProps} />
         <Toast />
       </PagesContextProvider>
+      <style jsx>{`
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+      `}</style>
     </SessionProvider>
   );
 };
