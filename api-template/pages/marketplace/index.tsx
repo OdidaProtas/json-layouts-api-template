4ef256a3376cccfd504b10551748cfe1ffa3d4c3
stilldrea <120886056+stilldrea@users.noreply.github.com>
2023-01-07 06:28:20 +0000

    update

--- api-template/pages/marketplace/index.tsx
+++ api-template/pages/marketplace/index.tsx
@@ -20,6 +20,8 @@ import {
   ListItem,
   ListItemButton,
   ListItemText,
+  Skeleton,
+  Stack,
 } from "@mui/material";
 import { Add, AppRegistration } from "@mui/icons-material";
 import useApps from "../../hooks/useApps";
@@ -27,35 +29,38 @@ import useCategories from "../../hooks/useCategories";
 import Layout from "../../components/Layout";
 import CaategoryDialog from "../../components/CategoryDialog";
 import App from "../../components/App";
+import { usePagesStateValue } from "../../lib/builder";
 
 const Marketplace: React.FC = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
-  const apps = useApps();
+  const apps = [];
 
   const userHasValidSession = Boolean(session);
 
   const categories = useCategories();
+  const loadingCategories = usePagesStateValue("loaders.categories");
+
   if (status === "loading") {
     return <AuthSpinner />;
   }
+
   return (
     <Layout>
       <div className="page">
-        <main>
-          <Container sx={{ display: "flex", mb: 9 }}>
-            <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ pb: 12 }}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Box sx={{ display: "flex" }}>
+              <Box sx={{ flexGrow: 1 }}>
+                <Typography sx={{ my: 3 }} variant="h5">
+                  Featured products
+                </Typography>
+              </Box>
               <Box sx={{ display: "flex" }}>
-                <Box sx={{ flexGrow: 1 }}>
-                  <Typography sx={{ my: 3 }} variant="h5">
-                    Featured products
-                  </Typography>
+                <Box>
+                  <CaategoryDialog appId={undefined} />
                 </Box>
-                <Box sx={{ display: "flex" }}>
-                  <Box>
-                    <CaategoryDialog appId={undefined} />
-                  </Box>
-                  {/* <Autocomplete
+                {/* <Autocomplete
                     size="small"
                     sx={{ width: 100, ml: 3 }}
                     fullWidth
@@ -73,145 +78,173 @@ const Marketplace: React.FC = () => {
                       <TextField size="small" {...params} placeholder="Search apps" />
                     )}
                   /> */}
-                </Box>
               </Box>
-              <Box sx={{ my: 1 }}>
-                <Grid container spacing={4}>
-                  <Grid
-                    item
-                    xs={3}
-                    sx={{
-                      display: { xs: "none", lg: "block" },
-                    }}
-                  >
-                    <Paper
-                      elevation={0}
-                      sx={{ p: 2, height: 400, overflow: "auto" }}
-                    >
-                      <Typography>Categories</Typography>
-                      <Divider sx={{ my: 2 }} />
-                      <List dense>
-                        {" "}
-                        {categories.map((cat) => {
-                          return (
-                            <ListItem key={cat.id}>
-                              <ListItemButton>
-                                <ListItemText primary={cat.name}></ListItemText>
-                              </ListItemButton>
-                            </ListItem>
-                          );
-                        })}
-                      </List>
-                    </Paper>
-                  </Grid>
-                  <Grid item xs={12} lg={9}>
-                    <Paper elevation={0} sx={{ width: "100%" }}>
-                      <Carousel
-                        showIndicators={false}
-                        showStatus={false}
-                        showArrows={false}
-                        showThumbs={false}
-                        autoPlay
-                        interval={2000}
-                        centerMode
-                        centerSlidePercentage={80}
-                      >
-                        {apps.map((app, index) => {
-                          return (
-                            <div key={index}>
-                              <img style={{ height: 400 }} src={app.image} />
-                            </div>
-                          );
-                        })}
-                      </Carousel>
-                    </Paper>
-                  </Grid>
-                </Grid>
-              </Box>
-              <Box sx={{ display: "flex", my: 12 }}>
-                <Typography
-                  sx={{ flexGrow: 1, display: { xs: "none", lg: "block" } }}
-                  variant="h5"
+            </Box>
+            <Box sx={{ my: 1 }}>
+              <Grid container spacing={4}>
+                <Grid
+                  item
+                  xs={3}
+                  sx={{
+                    display: { xs: "none", lg: "block" },
+                  }}
                 >
-                  All products
-                </Typography>
-                <Box>
-                  <Autocomplete
-                    size="small"
-                    disablePortal
-                    id="combo-box-demo"
-                    options={apps.map((app) => ({
-                      value: app.id,
-                      label: app.name,
-                    }))}
-                    sx={{ width: 300 }}
-                    onChange={(e, v) => {
-                      if ((v as any)?.value)
-                        router.push(`/${(v as any).value}`);
-                    }}
-                    renderInput={(params) => (
-                      <TextField
-                        size="small"
-                        {...params}
-                        placeholder="Search apps/categories..."
-                      />
-                    )}
-                  />
-                </Box>
-              </Box>
-              <Grid
-                sx={{ mb: 4, justifyContent: "center" }}
-                container
-                spacing={2}
-              >
-                <Grid item xs={10} md={9} lg={8}>
-                  <Grid container spacing={2}>
-                    {apps.map((app) => (
-                      <Grid key={app.id} item lg={3} md={6} xs={6}>
-                        <div className="post">
-                          <App product noStatus app={app} />
-                        </div>
-                      </Grid>
-                    ))}
-                  </Grid>
-                </Grid>
-
-                {!apps.length && (
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      minHeight: 300,
-                    }}
+                  <Paper
+                    elevation={0}
+                    sx={{ p: 2, height: 400, overflow: "auto" }}
                   >
-                    <div>
-                      <Typography variant="h4" sx={{ my: 3 }}>
-                        There are no published apps
-                      </Typography>
-                      <Button
-                        sx={{ textTransform: "none" }}
-                        variant={"outlined"}
-                        onClick={() => router.push("/create")}
-                        startIcon={<Add />}
-                      >
-                        Create app
-                      </Button>
-                      <Button
-                        startIcon={<AppRegistration />}
-                        sx={{ ml: 2, textTransform: "none" }}
-                        variant={"outlined"}
-                        onClick={() => router.push("/m")}
-                      >
-                        My apps
-                      </Button>
-                    </div>
-                  </div>
-                )}
+                    <Typography>Categories</Typography>
+                    <Divider sx={{ my: 2 }} />
+                    {loadingCategories && (
+                      <Stack spacing={2}>
+                        {[1, 2, 3, 4, 5].map((cat) => {
+                          return <Skeleton key={cat}></Skeleton>;
+                        })}
+                      </Stack>
+                    )}
+                    <List dense>
+                      {categories.map((cat) => {
+                        return (
+                          <ListItem key={cat.id}>
+                            <ListItemButton>
+                              <ListItemText primary={cat.name}></ListItemText>
+                            </ListItemButton>
+                          </ListItem>
+                        );
+                      })}
+                      {Boolean(!categories?.length) && !loadingCategories && (
+                        <Box
+                          sx={{
+                            height: "100%",
+                            display: "flex",
+                            alignItems: "center",
+                          }}
+                        >
+                          Error, Category data not added
+                        </Box>
+                      )}
+                    </List>
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} lg={7}>
+                  <Paper elevation={0} sx={{ width: "100%", height: "100%" }}>
+                    {!Boolean(apps?.length) && (
+                      <Skeleton
+                        animation="wave"
+                        height={"100%"}
+                        variant="rectangular"
+                      ></Skeleton>
+                    )}
+
+                    <Carousel
+                      showIndicators={false}
+                      showStatus={false}
+                      showArrows={false}
+                      showThumbs={false}
+                      autoPlay
+                      interval={2000}
+                      centerMode
+                      centerSlidePercentage={80}
+                    >
+                      {apps.map((app, index) => {
+                        return (
+                          <div key={index}>
+                            <img style={{ height: 400 }} src={app.image} />
+                          </div>
+                        );
+                      })}
+                    </Carousel>
+                  </Paper>
+                </Grid>
+                <Grid item xs lg={2}>
+                  <Paper elevation={0} sx={{ width: "100%", height: "100%" }}>
+                    {!Boolean(apps.length) && (
+                      <Skeleton
+                        animation="wave"
+                        height={"100%"}
+                        variant="rectangular"
+                      ></Skeleton>
+                    )}
+                  </Paper>
+                </Grid>
               </Grid>
             </Box>
-          </Container>
-        </main>
+            <Box sx={{ display: "flex", my: 12 }}>
+              <Typography
+                sx={{ flexGrow: 1, display: { xs: "none", lg: "block" } }}
+                variant="h5"
+              >
+                All products
+              </Typography>
+              <Box>
+                <Autocomplete
+                  size="small"
+                  disablePortal
+                  id="combo-box-demo"
+                  options={apps.map((app) => ({
+                    value: app.id,
+                    label: app.name,
+                  }))}
+                  sx={{ width: 300 }}
+                  onChange={(e, v) => {
+                    if ((v as any)?.value) router.push(`/${(v as any).value}`);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      size="small"
+                      {...params}
+                      placeholder="Search apps/categories..."
+                    />
+                  )}
+                />
+              </Box>
+            </Box>
+            <Grid
+              sx={{ mb: 4, justifyContent: "center" }}
+              container
+              spacing={2}
+            >
+              <Grid item xs={10} md={9} lg={8}>
+                <Grid container spacing={2}>
+                  {apps.map((app) => (
+                    <Grid key={app.id} item lg={3} md={6} xs={6}>
+                      <div className="post">
+                        <App product noStatus app={app} />
+                      </div>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+          {!apps.length && (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: 300,
+              }}
+            >
+              <div>
+                <Typography variant="h4" sx={{ my: 3 }}>
+                  Sorry, but no products have been added
+                </Typography>
+                <Stack spacing={2} sx={{ mb: 6 }}>
+                  <Button
+                    startIcon={<AppRegistration />}
+                    sx={{ ml: 2, textTransform: "none" }}
+                    variant={"outlined"}
+                    fullWidth
+                    onClick={() => router.back()}
+                  >
+                    Go back
+                  </Button>
+                </Stack>{" "}
+              </div>
+            </div>
+          )}
+        </Box>
       </div>
       <style jsx>{`
         .post {
