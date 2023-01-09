f4f96b65b8b347ae94c730188710e630c0a64b7f
Mike <122255788+mcd123r@users.noreply.github.com>
2023-01-09 10:48:50 +0000

    get focused values

--- api-template/components/Text.tsx
+++ api-template/components/Text.tsx
@@ -1,12 +1,22 @@
 import { Typography } from "@mui/material";
 import useDetail from "../hooks/useRow";
 
-export default function Text({ text, variant = "body1", api = {} }) {
-  const [row] = useDetail(api);
-  console.log(api);
-  return (
-    <Typography variant={variant as any}>
-      {JSON.stringify(row ?? null)}
-    </Typography>
-  );
+export default function Text({ text, variant = "body1", api = {} }: any) {
+  const [row, loadingRow] = useDetail(api);
+  console.log(api, row);
+
+  function getText() {
+    if (api?.id) {
+      if (loadingRow) {
+        return "...";
+      }
+      if (row) {
+        const mapState = api?.mapState;
+        return row[mapState?.text];
+      }
+      return "...";
+    }
+    return text;
+  }
+  return <Typography variant={variant as any}>{getText()}</Typography>;
 }
