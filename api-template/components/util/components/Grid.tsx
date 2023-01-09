43355df924afef7700015f9ece13602360d23064
Mike <122255788+mcd123r@users.noreply.github.com>
2023-01-09 14:40:56 +0000

    fix

--- api-template/components/util/components/Grid.tsx
+++ api-template/components/util/components/Grid.tsx
@@ -5,7 +5,7 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import renderComponents from "../renderComponents";
 import useTransformComponents from "../../../hooks/useTransformComponents";
-import { LinearProgress } from "@mui/material";
+import { Skeleton } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -15,16 +15,20 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-export default function BasicGrid({ components, spacing, api = {}, lg=6, md=6, xs=6 }) {
+export default function BasicGrid({ components, spacing, api = {}, lg=6, md=6, xs=6 }:any) {
   const [apiComponents, loading] = useTransformComponents(api);
   components = [...components, ...(apiComponents ?? [])];
   return (
     <Box sx={{ flexGrow: 1 }}>
-      {loading && (
-        <Box>
-          <LinearProgress />
-        </Box>
-      )}
+    {loading && Boolean(api?.id) && (
+          <Grid spacing={3}>
+            {[1, 2, 3, 4].map((item) => {
+              return <Grid xs={xs ?? true} md={md} lg={lg} key={item} item>
+                <Skeleton variant="rectangular" height="200" >
+              </Grid>
+            })}
+          </Grid>
+        )}
       <Grid container spacing={spacing}>
         {components.map((component, index) => {
           return (
